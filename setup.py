0b55b6a008812e1610de76b48d90dc0479297cb3
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2023-03-25 02:04:17 +0800

    alvistack/3.10.6
        git clean -xdf
        tar zcvf ../python-filelock_3.10.6.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-filelock.spec ../python-filelock_3.10.6-1.spec
        mv ../python*-filelock*3.10.6*.{gz,xz,spec,dsc} /osc/home\:alvistack/tox-dev-py-filelock-3.10.6/
        rm -rf ../python*-filelock*3.10.6*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='filelock',
+    version='3.10.6',
+    description='A platform independent file lock.',
+    maintainer_email='Bernát Gábor <gaborjbernat@gmail.com>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: The Unlicense (Unlicense)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System',
+    ],
+    extras_require={
+        'docs': [
+            'furo>=2022.12.7',
+            'sphinx-autodoc-typehints!=1.23.4,>=1.22',
+            'sphinx>=6.1.3',
+        ],
+        'testing': [
+            'covdefaults>=2.3',
+            'coverage>=7.2.2',
+            'pytest-cov>=4',
+            'pytest-timeout>=2.1',
+            'pytest>=7.2.2',
+        ],
+    },
+    packages=[
+        'filelock',
+    ],
+    package_dir={'': 'src'},
+)
