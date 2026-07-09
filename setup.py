ff037291c18e4c7419c9e0d51045a2dab5f7c543
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-07-09 20:09:05 +0800

    alvistack/3.29.7
        git clean -xdf
        tar zcvf ../python-filelock_3.29.7.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-filelock.spec ../python-filelock_3.29.7-1.spec
        cp ../python*-filelock*3.29.7*.{gz,xz,spec,dsc} /osc/home\:alvistack/tox-dev-py-filelock-3.29.7/
        rm -rf ../python*-filelock*3.29.7*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='filelock',
+    version='3.29.7',
+    description='A platform independent file lock.',
+    maintainer_email='Bernát Gábor <gaborjbernat@gmail.com>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System',
+    ],
+    packages=[
+        'filelock',
+        'filelock._soft_rw',
+    ],
+    package_dir={'': 'src'},
+)
